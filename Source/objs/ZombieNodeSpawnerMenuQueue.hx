0f6f5a84513edfe40269bd99072f1d771c176503
ciggbit <hercludes@gmail.com>
2016-02-11 22:05:23 -0500

    Added zombie node spawner menu queue etc

---
+++ Source/objs/ZombieNodeSpawnerMenuQueue.hx
@@ -0,0 +1,56 @@
+package objs;
+
+import openfl.display.Bitmap;
+import utils.Util;
+import utils.Vector2;
+import objs.ZombieNodeSpawner;
+import objs.Zombie;
+
+class ZombieNodeSpawnerMenuQueue {
+//private:
+  private static var backdrop : Bitmap = null;
+  private static var dim: Vector2 = new Vector2(192, 32);
+  private var node: ZombieNodeSpawner;
+  private var pos: Vector2;
+  private var prev_mouse: Bool;
+  private var queue: Array<Bitmap>;
+//public static:
+  public static function initialize() {
+    backdrop = new Bitmap(Data.zombie_spawner_queue);
+    backdrop.visible = false;
+    Layers.Add_Child(backdrop, Layers.Zombie_spawner);
+  }
+// public:
+  public function new(x_:Int, y_:Int, node_:ZombieNodeSpawner) {
+    pos = new Vector2(x_, y_);
+    queue = [];
+    backdrop.x = x_;
+    backdrop.y = y_;
+    backdrop.visible = true;
+    node = node_;
+    // -- reap zombies in queue --
+    var t_queue: Array<ZombieType> = node.Ret_Queue();
+    for ( q in t_queue ) {
+      push_queue(ZombieType.Normal); // todo, set this to actual type
+    }
+  }
+  public function destroy() {
+    while ( queue.length != 0 )
+      Layers.Rem_Child(queue.pop(), Layers.Zombie_spawner);
+    backdrop.visible = false;
+  }
+  public function pop_queue() {
+    Layers.Rem_Child(queue.pop(), Layers.Zombie_spawner);
+  }
+  public function push_queue(type:ZombieType) {
+      var t : Int = type.getIndex();
+      var bm : Bitmap = new Bitmap(Data.zombie_icons[t]);
+      bm.x = backdrop.x + queue.length*32;
+      bm.y = backdrop.y;
+      Layers.Add_Child(bm, Layers.Zombie_spawner);
+      queue.push(bm);
+  }
+  public function close() {
+    backdrop.visible = false;
+  }
+}
---
+++ Source/objs/ZombieNodeSpawnerMenuTab.hx
@@ -0,0 +1,69 @@
+package objs;
+
+import openfl.display.Bitmap;
+import utils.Util;
+import utils.Vector2;
+import objs.ZombieNodeSpawner;
+import objs.UnitSelector;
+import objs.Zombie;
+
+class ZombieNodeSpawnerMenuTab {
+//private:
+  private static var backdrop : Bitmap = null;
+  private static var goal_img : Bitmap = null;
+  private static var dim: Vector2 = new Vector2(50, 160);
+  private static var button_dim: Vector2 = new Vector2(50, 32);
+  private var node: ZombieNodeSpawner;
+  private var pos: Vector2;
+  private var dragging: Bool;
+//public static:
+  public static function initialize() {
+    backdrop = new Bitmap(Data.zombie_spawner_tab);
+    backdrop.visible = false;
+    goal_img = new Bitmap(Data.zombie_spawn_tab_goal);
+    goal_img.visible = false;
+    Layers.Add_Child(backdrop, Layers.Zombie_spawner);
+    Layers.Add_Child(goal_img, Layers.Zombie);
+  }
+// public:
+  public function new(x_:Int, y_:Int, node_:ZombieNodeSpawner) {
+    node = node_;
+    pos = new Vector2(x_, y_);
+    dragging = false;
+    backdrop.x = x_;
+    backdrop.y = y_;
+    backdrop.visible = true;
+    goal_img.visible = true;
+    goal_img.x = node.Ret_Spawn_Goal().x;
+    goal_img.y = node.Ret_Spawn_Goal().y;
+  }
+  public function destroy() {
+    backdrop.visible = false;
+    goal_img.visible = false;
+  }
+  public function click() : Void {
+    if ( Util.PointCol(pos, dim, Input.mouse_pos) ) {
+      var t: Vector2 = new Vector2(Std.int(pos.x), Std.int(pos.y));
+      if ( Util.PointCol(t, button_dim, Input.mouse_pos) ) {
+        GameManager.zombie_spawner_menu.destroy();
+        return;
+      }
+      t.y += 32;
+      if ( Util.PointCol(t, button_dim, Input.mouse_pos) ) {
+        dragging = true;
+        GameManager.unit_select.clear_select();
+      }
+    }
+  }
+  public function update() : Void {
+    dragging = dragging && Input.mouse[Input.Mouse_left];
+    if ( dragging ) {
+      goal_img.x = Input.mouse_x - 8;
+      goal_img.y = Input.mouse_y - 8;
+      node.Set_Spawn_Goal(new Vector2(Input.mouse_x, Input.mouse_y));
+    }
+  }
+  public function close() {
+    backdrop.visible = false;
+  }
+}
